a47897918069de0ab4464fd7060bba6a2493565b
Paulo Vitor <paulo19032004@gmail.com>
2023-07-07 15:45:52 -0300

    First Commit

---
+++ src/test/java/com/updeploy/qrcode/QrCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.updeploy.qrcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
